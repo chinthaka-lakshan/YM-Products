b967a6542fc5db1dd890367a53b9313a1ee26eec
yashodashyaminda <yashodashaminda249@gmail.com>
2025-03-29 00:44:25 +0530

    Orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css';
 import Login from '../src/Pages/Login/login.jsx';
 import Sidebar from '../src/components/Sidebar/Sidebar.jsx'; 
 import Dashboard from './Pages/Dashboard/Dashboard.jsx';
+import Orders from './Pages/Orders/Orders.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/dashboard" element={<Dashboard/>} /> {/* Assuming Sidebar is the main component for the dashboard */}
+          <Route path='/orders' element={<Orders/>} /> {/* Assuming Sidebar is the main component for the orders */}
         </Routes>
       </div>
     </Router>
---
+++ frontend/src/Pages/Orders/Orders.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../../components/Sidebar/Sidebar'
+
+const Orders = () => {
+  return (
+    <div className="container">
+        <Sidebar/>
+
+    </div>
+
+    
+  )
+}
+
+export default Orders
