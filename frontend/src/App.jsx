24c89e8af405691762be50d1e6f05f9df16560e7
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2025-03-28 22:08:35 +0530

    fix app.jsx and side bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './Pages/Login/login.jsx'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import './App.css';
+import Login from '../src/Pages/Login/login.jsx';
+import Sidebar from '../src/components/Sidebar/Sidebar.jsx'; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <Login/>
-    </div>
-  )
+    <Router> {/* Router should wrap Routes */}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sidebar" element={<Sidebar />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
