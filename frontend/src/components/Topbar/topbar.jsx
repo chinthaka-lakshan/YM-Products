cf388ba306935a793d8240e5c6340d3d1bb0a2b4
Hasitha <hasithawijewardhana19@gmail.com>
2025-03-31 23:35:19 +0530

    problem slove

--- frontend/src/components/Topbar/topbar.jsx
+++ frontend/src/components/Topbar/topbar.jsx
@@ -1,10 +1,16 @@
+import React, { useState } from "react";
 import "./topBar.css";
 import { FaUserCircle } from "react-icons/fa";
-import { FiMenu } from "react-icons/fi"; // Mobile menu icon
+import { FiMenu } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi"; // Logout icon
+import Sidebar from "../Sidebar/Sidebar";
 
 const TopBar = () => {
+  const [showLogout, setShowLogout] = useState(false);
+
   return (
     <div className="topbar">
+      <Sidebar/>
       {/* Left side - Logo */}
       <div className="topbar-left">
         <span className="logo">
@@ -14,15 +20,24 @@ const TopBar = () => {
 
       {/* Right side - Admin Info */}
       <div className="topbar-right">
-        <div className="admin-info">
+        {/* Mobile Menu Icon */}
+        <div className="menu-icon">
+          <FiMenu />
+        </div>
+
+        {/* User Icon */}
+        <div className="user-container" onClick={() => setShowLogout(!showLogout)}>
           <FaUserCircle className="admin-icon" />
           <span className="admin-name">Admin</span>
+          
+          {/* Logout option (hidden until clicked) */}
+          {showLogout && (
+            <div className="logout-dropdown">
+              <FiLogOut className="logout-icon" />
+              <span className="logout-text">Log Out</span>
+            </div>
+          )}
         </div>
-      </div>
-
-      {/* Mobile Menu Icon */}
-      <div className="menu-icon">
-        <FiMenu />
       </div>
     </div>
   );
