fd207841ebca2ddbad3928b4dff26ddae8f19bec
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2025-03-31 23:02:20 +0530

    repregistretion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './Pages/Login/AdminLogin/login.jsx'; // Import the AdminLogin component
 import RepLogin from './Pages/Login/RepLogin/RepLogin.jsx'; // Import the RepLogin component
+import RepRegistration from './Pages/RepRegistration/RepRegistration.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/replogin" element={<RepLogin />} />
+          <Route path="/repredistration" element={<RepRegistration/>} />
         </Routes>
       </div>
     </Router>
