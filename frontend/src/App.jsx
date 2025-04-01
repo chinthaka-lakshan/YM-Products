33daa12e13d465018b77421a5ca43d96e23bd3d4
Sachintha Rashen Bandara <srashenb@gmail.com>
2025-04-01 23:52:35 +0530

    Revised Sidebar & Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './Pages/Login/AdminLogin/login.jsx';
 import RepLogin from './Pages/Login/RepLogin/RepLogin.jsx';
-import RepRegistration from './Pages/RepRegistration/RepRegistration.jsx';
-import AdminDashboard from './Pages/Dashboard/AdminDashboard/AdminDashboard.jsx';
+import AdminDashboard from './Pages/AdminDashboard/AdminDashboard.jsx';
+import DistributionStock from './Pages/DistributionStock/DistributionStock.jsx';
+import PurchaseStock from './Pages/PurchaseStock/PurchaseStock.jsx';
 import Orders from './Pages/Orders/Orders.jsx';
-import RepDashboard from './Pages/Dashboard/RepDashboard/RepDashboard.jsx';
+import AdminShops from './Pages/AdminShops/AdminShops.jsx';
+import RepRegistration from './Pages/RepRegistration/RepRegistration.jsx';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <Router> 
+    <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/admindashboard" element={<AdminDashboard/>} />
-          <Route path="/orders" element={<Orders/>} />
-          <Route path="/replogin" element={<RepLogin />} />
-          <Route path="/repredistration" element={<RepRegistration/>} />
-          <Route path="/repdashboard" element={<RepDashboard/>} />
-          <Route path="/repregistration" element={<RepRegistration/>} />
+          <Route path="/" element={<Login />}/>
+          <Route path="/adminDashboard" element={<AdminDashboard/>}/>
+          <Route path="/distributionStock" element={<DistributionStock/>}/>
+          <Route path="/purchaseStock" element={<PurchaseStock/>}/>
+          <Route path="/adminOrders" element={<Orders/>}/>
+          <Route path="/adminShops" element={<AdminShops/>}/>
+          <Route path="/salesReps" element={<RepRegistration/>}/>
+
+          <Route path="/repLogin" element={<RepLogin />}/>
         </Routes>
       </div>
     </Router>
