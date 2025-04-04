9009a50399a854d8e565044227d025e59c43d8ad
LAKSHAN M.J.C.D. <diluklakshan01@gmail.com>
2025-04-04 15:59:16 +0530

    fix

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -38,12 +38,12 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-'api' => [
-    \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-    'throttle:api',
-    \Illuminate\Routing\Middleware\SubstituteBindings::class,
-],
-    ];
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+            ];
 
     /**
      * The application's route middleware.
