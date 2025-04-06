a23b0abf62e684e144b24b1c24579a4a3edc7e3a
yashodashyaminda <yashodashaminda249@gmail.com>
2025-04-06 21:33:07 +0530

    create returns php class

---
+++ backend/database/migrations/2025_04_06_093730_create_returns_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('returns', function (Blueprint $table) {
+            $table->id();
+            $table->string("shop");
+            $table->string("item");
+            $table->integer("weight");
+            $table->integer("qty");  
+            $table->string("type");
+            $table->string("date");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('returns');
+    }
+};
