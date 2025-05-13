907ee37a0c103bb8ca605c0d361abe2c905b3620
Sachintha Rashen Bandara <srashenb@gmail.com>
2025-05-13 05:50:44 +0530

    Order To Edit Table

--- frontend/src/Pages/Login/AdminLogin/login.jsx
+++ frontend/src/Pages/Login/AdminLogin/login.jsx
@@ -8,7 +8,6 @@ const AdminLogin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-
   const [error, setError] = useState("");
   const [otp, setOTP] = useState("");
   const [newPassword, setNewPassword] = useState("");
